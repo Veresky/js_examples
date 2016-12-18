c2019ff123a1d60472d00954862fe0e7ffbb2634
Vitaly Zorin <oceanicwinds@gmail.com>
2016-12-18 17:43:07 +0400

    --

--- 08_array_behavior/main.js
+++ 08_array_behavior/main.js
@@ -1,22 +1,26 @@
 'use strict';
 
-var a = new Array(3);
-var b = [undefined, undefined, undefined];
-var c = [];
-c.length = 3;
-console.log(a);
-console.log(b);
-console.log(c);
+var h_step = 0.1;
+var y0 = 1;
+function baseFunc(x, y) {
+  return x + y
+}
 
-console.log('array comparison:')
+function findNextY(xN, yN, h) {
+  var k1 = baseFunc(xN, yN);
+  var k2 = baseFunc(xN + h / 2, yN + h * k1 / 2);
+  var k3 = baseFunc(xN + h / 2, yN + h * k2 / 2);
+  var k4 = baseFunc(xN + h, yN + h * k3);
+  return yN + h / 6 * (k1 + 2 * k2 + 2 * k3 + k4);
+}
 
-var a = new Array([1]);
-var b = new Array([1]);
+function findAllY(startX, endX, h, y0) {
+  var yPrev = y0;
+  for (var n = startX; n <= endX; n += h) {
+    var yN = findNextY(n, yPrev, h);
+    console.log(yN);
+    yPrev = yN;
+  }
+}
 
-console.log(a + ""== b + "", a + "", b + "");
-
-var q = new Boolean(false);
-var w = new Number(0);
-var e = new String("");
-
-console.log( q && w && e);
+findAllY(0, 0.5, h_step, y0);
