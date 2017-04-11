71f0c5f74dd15e8de3d2a2d38d288b51b0839c0d
Vitaly Zorin <zorin@competentum.ru>
2017-04-11 18:02:29 +0400

    +

--- 14_es2015/main.js
+++ 14_es2015/main.js
@@ -14,3 +14,39 @@ var [a, b, ...other] = simpleArray;
 console.log(a);
 console.log(b);
 console.log(other);
+
+
+// function defer(f, ms) {
+//   return function() {
+//     setTimeout(() => f.apply(this, arguments), ms)
+//   }
+// }
+
+// function defer(f, ms) {
+//   return function() {
+//     let args = arguments;
+//     let ctx = this;
+//     setTimeout(function() {
+//       return f.apply(ctx, args);
+//     }, ms);
+//   }
+// }
+
+
+function defer(f, ms) {
+  return function() {
+    let args = arguments;
+    let ctx = this;
+    setTimeout(function() {
+      f.apply(ctx, args);
+    }, ms);
+  }
+}
+
+function sayHi(who) {
+  alert('Привет, ' + who);
+}
+
+debugger;
+let sayHiDeferred = defer(sayHi, 2000);
+sayHiDeferred("Вася"); // Привет, Вася через 2 секунды
